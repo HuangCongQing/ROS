134d2e49c7234b40a06763604207b453c9f06ea1
HuangCongQing <1756260160@qq.com>
2020-07-14 16:01:10 +0000

    init

---
+++ catkin_wp/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/AzureUser/hcq/ROS/catkin_wp/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
---
+++ catkin_wp/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/AzureUser/hcq/ROS/catkin_wp/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/AzureUser/hcq/ROS/catkin_wp/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/AzureUser/hcq/ROS/catkin_wp/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/AzureUser/hcq/ROS/catkin_wp/src:$ROS_PACKAGE_PATH"
